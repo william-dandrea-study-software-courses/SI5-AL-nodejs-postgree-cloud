52847c60310bd92c6261ad84671c69b58e1580cf
William D'Andrea <71140800+william-dandrea@users.noreply.github.com>
2022-09-13 17:06:17 +0200

    Final version of Heroku app with database and docker

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const { Client } = require("pg");
 
+/*
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
-});
+}); */
 
-/*
+
 const client = new Client({
-        user: process.env.USER_DB,
-        host: process.env.HOST_DB,
-        database: process.env.DATABASE_NAME,
-        password: process.env.PASSWORD_DB,
-        port: process.env.PORT_DB
-    });
-}*/
+    user: process.env.USER_DB,
+    host: process.env.HOST_DB,
+    database: process.env.DATABASE_NAME,
+    password: process.env.PASSWORD_DB,
+    port: process.env.PORT_DB
+})
+
 
 
 
